46689b09ba862e44b2e8e2eac954848efb89f27e
Owen <kuemerle5@hotmail.com>
2014-04-13 07:07:01 -0700

    Music widget component. Wow. Such nice

---
+++ music_playback/music.js
@@ -0,0 +1,14 @@
+//var safeTrackData = [];
+//
+//(function() {
+//    $.getJSON("http://api.soundcloud.com/playlists/405726/tracks.json?client_id=498a8a1312c538b970408600153ae43e", function(json) {
+//        var tracksResult = [];
+//        console.log(json.length);
+//        console.log("Stuff is happening!");
+//        json.forEach( function(element, index, array) {
+//            console.log(element);
+//            tracksResult.push(element);
+//        });
+//        safeTrackData = tracksResult;
+//    });
+//})();
